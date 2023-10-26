23e5155f4213d9ea88f8c487e86ab504ddae98af
Amy Martin <amymartinm97@ucla.edu>
2023-10-25 22:31:27 -0700

    Added preliminary navbar component and font imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import './App.css';
+import { NavBar } from './components/NavBar';
 
 const App = () => {
   return (
     <Router>
-      {/* <NavBar /> */}
+      <NavBar />
       <main>
         {/* <NavBarSideBar /> */}
         <Routes>
