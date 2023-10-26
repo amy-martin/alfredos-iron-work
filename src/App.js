6d83af70b3d6415a933e0017407f71b8c0a81b74
Amy Martin <amymartinm97@ucla.edu>
2023-10-25 22:14:39 -0700

    Added general component outline to main app component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 
-function App() {
-
+const App = () => {
+  return (
+    <Router>
+      {/* <NavBar /> */}
+      <main>
+        {/* <NavBarSideBar /> */}
+        <Routes>
+          {/* <Route path='/' element={<HomePage />}/>
+          <Route path='/services' element={<Services />}/>
+          <Route path='/about' element={<AboutUs />}/>
+          <Route path='/contact' element={<ContactUs />}/> */}
+        </Routes>
+      </main>
+    </Router>
+  )
 }
-
 export default App;
