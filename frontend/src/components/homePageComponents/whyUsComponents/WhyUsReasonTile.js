e948b4e2b82ab852a9fca521fea2da72b1ac7460
Amy Martin <amymartinm97@ucla.edu>
2023-11-01 14:07:31 -0700

    Finalized 'Why Us' list styling

--- frontend/src/components/homePageComponents/whyUsComponents/WhyUsReasonTile.js
+++ frontend/src/components/homePageComponents/whyUsComponents/WhyUsReasonTile.js
@@ -1,12 +1,16 @@
 
-import React from "react";
+import React, { useState } from "react";
 
 export const WhyUsReasonTile = (props) => {
     const {imgSrc, reason, reasonExcerpt} = props.reason;
+    const [display, setDisplay] = useState('none')
+    const handleClick = () => {
+        setDisplay((display === 'none' ? 'flex': 'none'))
+    }
     return (
-        <li>
-            <div className="why-us-list-content"><img src={imgSrc}/><p className="reason-title">{reason}</p></div>
-            <p className="reason-excerpt" style={{display:'none'}}>{reasonExcerpt}</p>
+        <li className='why-us-list-content' onClick={handleClick}>
+            <div className="baseline-content"><div className="icon-container"><img src={imgSrc}/></div><p className="reason-title">{reason}</p></div>
+            <p className="reason-excerpt" style={{display:`${display}`}}>{reasonExcerpt}</p>
         </li>
     )
 }
