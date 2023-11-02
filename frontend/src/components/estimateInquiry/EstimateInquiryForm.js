adcc64a96f0cda71109d01d48c70fa8c2558d968
Amy Martin <amymartinm97@ucla.edu>
2023-11-02 12:48:46 -0700

    Added state variables to keep track of form data

--- frontend/src/components/estimateInquiry/EstimateInquiryForm.js
+++ frontend/src/components/estimateInquiry/EstimateInquiryForm.js
@@ -1,7 +1,25 @@
 import React from "react";
 import { serverAddress } from "../../App";
+import { on } from "nodemailer/lib/xoauth2";
 
 export const EstimateInquiryForm = () => {
+
+    
+    const [formData, setFormData] = useState({
+        name: '',
+        phone: '',
+        email: '',
+        service: '',
+        additionalDetails: '',
+    });
+    const handleChange = (e) => {
+        // Update the state when form fields change
+        const { name, value } = e.target;
+        setFormData({
+          ...formData,
+          [name]: value,
+        });
+      };
     
     const onSubmit = async () => {
         const options = {
@@ -13,29 +31,29 @@ export const EstimateInquiryForm = () => {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-
+                formData
             })
         }
         await fetch(`${serverAddress}/submit-inquiry`, )
     }
     return (
-            <form className="estimate-inquiry-form" >
+            <form className="estimate-inquiry-form" onSubmit={onSubmit}>
                 <div className="form-elements">
                     <div className="form-element">
                         <label for='name'>Full Name:</label>
-                        <input type='text' name='name' id='name' required></input>
+                        <input type='text' name='name' id='name' required onChange={handleChange} />
                     </div>
                     <div className="form-element">
                         <label for='phone'>Phone:</label>
-                        <input type='tel' name='phone' id='phone' required></input>
+                        <input type='tel' name='phone' id='phone' required onChange={handleChange} />
                     </div>
                     <div className="form-element">
                         <label for='email'>Email:</label>
-                        <input type='email' name='email' id='email' required></input>
+                        <input type='email' name='email' id='email' required onChange={handleChange} />
                     </div>
                     <div className="form-element">
                         <label for='service'>Service Needed:</label>
-                        <select type='text' name='service' id='service' required>
+                        <select type='text' name='service' id='service' required onChange={handleChange}>
                             <option disabled selected value="">Select an option</option>
                             <option value="">Balcony</option>
                             <option value="">Railing</option>
@@ -50,7 +68,7 @@ export const EstimateInquiryForm = () => {
                     </div>
                     <div className="form-element">
                         <label for='additional-details'>Additional Details:</label>
-                        <textarea type='text' name='additional-details' id='additional-details' rows='3' cols='50' required />
+                        <textarea type='text' name='additional-details' id='additional-details' rows='3' cols='50' required onChange={handleChange}/>
                     </div>
                 </div>
                 <button type="submit">Send Inquiry</button>
