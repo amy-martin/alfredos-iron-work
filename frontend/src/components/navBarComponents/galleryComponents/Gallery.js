4ee240ffcad618d74eaf1e666ee69ebae6b53bbe
Amy Martin <amymartinm97@ucla.edu>
2023-11-08 12:04:25 -0800

    Added gallery component

---
+++ frontend/src/components/navBarComponents/galleryComponents/Gallery.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Gallery = () => {
+    return (
+        <div className="gallery">
+            <h2 className="title">GALLERY</h2>
+        </div>
+    )
+}
