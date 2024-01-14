b8f0bad802ad007cb887c2b56207d1d7a605b323
Amy Martin <amymartinm97@ucla.edu>
2024-01-14 15:53:30 -0500

    Sidebar icon styling fix

--- frontend/src/components/navBarComponents/NarrowScreenNavBar.js
+++ frontend/src/components/navBarComponents/NarrowScreenNavBar.js
@@ -1,17 +1,40 @@
-import React from "react";
-import { useDispatch } from "react-redux";
-import {toggleSidebarDisplay} from '../../slices/navSidebarSlice'
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {toggleSidebarDisplay, selectNavSidebarDisplay} from '../../slices/navSidebarSlice'
 import { NavLink } from "react-router-dom";
 
+
 export const NarrowScreenNavBar = () => {
-    const dispatch = useDispatch()
+    const sidebarDisplay = useSelector(selectNavSidebarDisplay)
+    const dispatch = useDispatch();
+    const [isActive, setIsActive] = useState(false);
+
+    useEffect(() => {
+        console.log(sidebarDisplay === 'flex')
+        if (sidebarDisplay === 'flex') {
+            console.log(sidebarDisplay)
+            
+            setIsActive(true)
+        } else {
+            setIsActive(false)
+        }
+    }, [sidebarDisplay])
     const handleClick = () => {
         dispatch(toggleSidebarDisplay());
     }
     return (
         <nav className="narrow-screen navbar" >
             <NavLink  to={"/"}><img className="site-logo"src={require('../../media/aiw-logo-small.png')}/></NavLink>
-            <img className="sidebar-icon" src={require('../../media/sidebar-icon.png')} onClick={handleClick}/>
+            <div className={`sidebar-icon-container ${isActive ? "active" : ""}`} onClick={handleClick}>
+                <div className="sidebar-icon">
+
+                        <hr className="line-1"/>
+
+                        <hr className="line-2"/>
+
+                        <hr className="line-3"/>
+                </div>
+            </div>
         </nav>
     )
 }
